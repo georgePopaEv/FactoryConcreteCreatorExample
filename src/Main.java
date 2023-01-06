bbfad1815cf545f61ece946e4c1e1fe83bf611a4
GeorgePopaA <poplike14@yahoo.com>
2023-01-06 14:06:51 +0200

    Concrete Creator Factory Example

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+
+
+interface Shape{
+    void draw();
+}
+
+class Rectangle implements Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+}
+
+class Square implements Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
+
+class Circle implements Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
